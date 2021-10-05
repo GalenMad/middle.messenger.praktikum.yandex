e9ca6689c908b6d4c7be02a6e1c8f78a6199877d
Anton Voronkov <amvoronkov@yandex.ru>
2021-10-05 22:23:28 +0300

    base pages structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,8 @@
-export default pages = {
-    '404': import('./404/index.pug')
-}
+// const pages = {
+//     'authorization': import('./authorization/index.pug'),
+//     'registartion': import('./registartion/index.pug'),
+//     'user-settings': import('./user-settings/index.pug'),
+//     'main': import('./main/index.pug'),
+//     '404': import('./404/index.pug'),
+//     '500': import('./500/index.pug')
+// }
