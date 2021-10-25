2d43b29b8fa9d0b07355b69e3424413087269834
Anton Voronkov <amvoronkov@yandex.ru>
2021-10-25 23:22:22 +0300

    clearing structure for routing

--- src/app.js
+++ src/app.js
@@ -1,4 +1,2 @@
 
-import compileTemplate from './pages/user-settings/script.js';
-let list = ['authorization', 'registartion', 'main', 'user-settings', '404', '500'];
-document.querySelector('#app').innerHTML = compileTemplate()
+import './styles.scss';
