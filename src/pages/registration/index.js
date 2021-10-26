6936b936000699686b6cddab77fd647f66afc24c
Anton Voronkov <amvoronkov@yandex.ru>
2021-10-26 21:04:39 +0300

    upd registration page

--- src/pages/registration/index.js
+++ src/pages/registration/index.js
@@ -1,5 +1,55 @@
 import '../../mixins/form-page/styles.scss';
 import '../../mixins/form-group/styles.scss';
 import compileTemplate from './template.pug'
-const compile = (props) => compileTemplate(props);
-export default compile;
+
+const formGroups = [{
+    label: 'Почта',
+    id: 'email',
+    inputType: 'email',
+    validationMessage: 'Неправильный что-то там',
+    isRequired: true
+},
+{
+    label: 'Логин',
+    id: 'login',
+    inputType: 'text',
+    validationMessage: 'Неправильный что-то там',
+    isRequired: true
+},
+{
+    label: 'Имя',
+    id: 'first_name',
+    inputType: 'text',
+    validationMessage: 'Неправильный что-то там',
+    isRequired: true
+},
+{
+    label: 'Фамилия',
+    id: 'last_name',
+    inputType: 'text',
+    validationMessage: 'Неправильный что-то там',
+    isRequired: true
+},
+{
+    label: 'Телефон',
+    id: 'phone',
+    inputType: 'tel',
+    validationMessage: 'Неправильный что-то там',
+    isRequired: true
+},
+{
+    label: 'Пароль',
+    id: 'password',
+    inputType: 'password',
+    validationMessage: 'Неправильный что-то там',
+    isRequired: true
+},
+{
+    label: 'Повторите пароль',
+    id: 'repeatPassword',
+    inputType: 'password',
+    validationMessage: 'Неправильный что-то там',
+    isRequired: true
+}]
+
+export default (props) => compileTemplate(Object.assign({ formGroups }, props));
