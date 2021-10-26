8c50eb57de52a5d4098ce035306c4ba10710af66
Anton Voronkov <amvoronkov@yandex.ru>
2021-10-26 20:18:25 +0300

    upd user-settings

--- src/pages/user-settings/index.js
+++ src/pages/user-settings/index.js
@@ -1,5 +1,26 @@
 import './styles.scss';
 import '../../mixins/link/styles.scss';
 import compileTemplate from './template.pug'
-const compile = (props) => compileTemplate(props);
+
+const userData = [{
+    name: 'Почта',
+    value: 'pochta@yandex.ru',
+},{
+    name: 'Логин',
+    value: 'ivanivanov',
+},{
+    name: 'Имя',
+    value: 'Иван',
+},{
+    name: 'Фамилия',
+    value: 'Иванов',
+},{
+    name: 'Имя в чате',
+    value: 'Иван',
+},{
+    name: 'Телефон',
+    value: '+7 (909) 967 30 30'
+}];
+
+const compile = (props) => compileTemplate(Object.assign({userData}, props));
 export default compile;
