6a24d30ca478b14625c958a0ee252d4c78e57d57
Anton Voronkov <amvoronkov@yandex.ru>
2021-10-28 00:28:04 +0300

    upd chats-page

--- src/pages/chats-page/index.js
+++ src/pages/chats-page/index.js
@@ -2,12 +2,16 @@ import './styles.scss'
 import compileTemplate from './template.pug'
 import avatar from '../../assets/images/default-avatar.svg'
 
-const chats = [{
-    avatar,
-    chatName: 'Андрей',
-    chatTeaser: 'И Human Interface Guidelines и Material Design рекомендуют…',
-    lastMessageTime: '10:20',
-    newMessageCount: 2
-}];
+const chats = [];
+
+for (let i = 0; i < 10; i++) {
+    chats.push({
+        avatar,
+        chatName: 'Андрей',
+        chatTeaser: 'И Human Interface Guidelines и Material Design рекомендуют…',
+        lastMessageTime: '10:20',
+        newMessageCount: 2
+    });
+}
 
 export default (props) => compileTemplate(Object.assign({ chats, userAvatar: avatar }, props));
