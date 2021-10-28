5d17fb86c28a718870ec2891c72bebc6360fefe4
Anton Voronkov <amvoronkov@yandex.ru>
2021-10-28 22:17:53 +0300

    fix st errors

--- src/app.ts
+++ src/app.ts
@@ -4,16 +4,21 @@ import pages from './pages';
 import { SpreadPage } from './pages';
 import Router from './Router';
 
-// Router нужен, чтобы не плодить лишних html-страниц
-const root = document.querySelector('#app');
+const root: HTMLElement | null = document.querySelector('#app');
+
+if (root === null) {
+	throw new Error('Не найден root-элемент');
+}
+
+// Router нужен, чтобы не плодить лишние html-страниц
 const router = new Router({
 	mode: 'hash',
 	root: '/'
 });
 
-const list = Object.entries(pages).map(item => {
-	const { path, name } = item[1];
-	return { path: `/${path}`, name };
+
+const list = Object.values(pages).map((item: { path: string, name: string }) => {
+	return { path: item.path, name: item.name };
 });
 
 for (const page in pages) {
