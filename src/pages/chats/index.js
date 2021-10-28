b9749ec89f70f055cdecfbc59f57378a6ce69921
Anton Voronkov <amvoronkov@yandex.ru>
2021-10-28 22:51:34 +0300

    add new chat item structure

--- src/pages/chats/index.js
+++ src/pages/chats/index.js
@@ -6,12 +6,23 @@ const chats = [];
 
 for (let i = 0; i < 4; i++) {
 	chats.push({
-		avatar,
-		chatName: 'Андрей',
-		chatTeaser: 'И Human Interface Guidelines и Material Design рекомендуют не выпендриваться…',
-		lastMessageTime: '10:20',
-		newMessageCount: i + 2
-	});
+		"id": 123,
+		"title": "Андрей",
+		"avatar": avatar,
+		"unread_count": i + 2,
+		"last_message": {
+			"user": {
+				"first_name": "Petya",
+				"second_name": "Pupkin",
+				"avatar": avatar,
+				"email": "my@email.com",
+				"login": "userLogin",
+				"phone": "8(911)-222-33-22"
+			},
+			"time": "10:20",
+			"content": "И Human Interface Guidelines и Material Design рекомендуют не выпендриваться…"
+		}
+	})
 }
 
 export default (props) => compileTemplate(Object.assign({ chats, userAvatar: avatar }, props));
