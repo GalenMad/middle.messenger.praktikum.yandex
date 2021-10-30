1bb1e127949391c2e52a9149562e19fbd3fb16d4
Anton Voronkov <amvoronkov@yandex.ru>
2021-10-30 19:51:11 +0300

    input.js

---
+++ src/components/input/index.js
@@ -0,0 +1,30 @@
+import Block from '../../modules/block';
+
+class Input extends Block {
+	constructor(props) {
+		super('input', props);
+		const { validators } = props;
+		this._meta.validators = validators ? validators : {};
+	}
+
+	get value() {
+		return this.element.value;
+	}
+
+	get validators() {
+		return this._meta.validators;
+	}
+
+	get validity() {
+		const validity = {};
+		const value = this.value;
+
+		Object.keys(this.validators).map(validatorName => {
+			validity[validatorName] = this.validators[validatorName](value);
+		});
+
+		return validity;
+	}
+}
+
+export default Input;
