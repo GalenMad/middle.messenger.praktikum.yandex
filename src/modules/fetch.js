aa05e12a51b87f687a1996cc3551cc3b78ecd6da
Anton Voronkov <amvoronkov@yandex.ru>
2021-11-03 02:47:16 +0300

    stranger things

--- src/modules/fetch.js
+++ src/modules/fetch.js
@@ -1,63 +1,63 @@
 const stringifyQuery = (data) => {
-  if (!data || typeof data !== 'object') {
-    return '';
-  }
-  return Object.entries(data).reduceRight((prev, curr) => `${curr[0]}=${curr[1].toString()}${prev ? `&${prev}` : ''}`, null);
+	if (!data || typeof data !== 'object') {
+		return '';
+	}
+	return Object.entries(data).reduceRight((prev, curr) => `${curr[0]}=${curr[1].toString()}${prev ? `&${prev}` : ''}`, null);
 };
 
 const METHODS = {
-  GET: 'GET',
-  POST: 'POST',
-  PUT: 'PUT',
-  PATCH: 'PATCH',
-  DELETE: 'DELETE',
+	GET: 'GET',
+	POST: 'POST',
+	PUT: 'PUT',
+	PATCH: 'PATCH',
+	DELETE: 'DELETE',
 };
 
 class HTTPTransport {
 	get = (url, options = {}) => {
-	  const { data = {} } = options;
-	  this.request(`${url}?${stringifyQuery(data) || ''}`, { ...options, method: METHODS.GET }, options.timeout);
+		const { data = {} } = options;
+		this.request(`${url}?${stringifyQuery(data) || ''}`, { ...options, method: METHODS.GET }, options.timeout);
 	};
 
 	put = (url, options = {}) => {
-	  this.request(url, { ...options, method: METHODS.PUT }, options.timeout);
+		this.request(url, { ...options, method: METHODS.PUT }, options.timeout);
 	};
 
 	post = (url, options = {}) => {
-	  this.request(url, { ...options, method: METHODS.POST }, options.timeout);
+		this.request(url, { ...options, method: METHODS.POST }, options.timeout);
 	};
 
 	delete = (url, options = {}) => {
-	  this.request(url, { ...options, method: METHODS.DELETE }, options.timeout);
+		this.request(url, { ...options, method: METHODS.DELETE }, options.timeout);
 	};
 
 	request = (url, options, timeout = 10000) => {
-	  const { method, headers = {}, data } = options;
-	  if (method === METHODS.GET && data) {
-	    url += `?${stringifyQuery(data)}`;
-	  }
-	  return new Promise((resolve, reject) => {
-	    const xhr = new XMLHttpRequest();
-	    xhr.open(method, url);
+		const { method, headers = {}, data } = options;
+		if (method === METHODS.GET && data) {
+			url += `?${stringifyQuery(data)}`;
+		}
+		return new Promise((resolve, reject) => {
+			const xhr = new XMLHttpRequest();
+			xhr.open(method, url);
 
-	    if (headers) {
-	      for (const header in headers) {
-	        xhr.setRequestHeader(header, headers[header]);
-	      }
-	    }
-	    xhr.timeout = timeout;
+			if (headers) {
+				for (const header in headers) {
+					xhr.setRequestHeader(header, headers[header]);
+				}
+			}
+			xhr.timeout = timeout;
 
-	    xhr.onload = () => resolve(xhr);
-	    xhr.onabort = reject;
-	    xhr.onerror = reject;
-	    xhr.ontimeout = reject;
+			xhr.onload = () => resolve(xhr);
+			xhr.onabort = reject;
+			xhr.onerror = reject;
+			xhr.ontimeout = reject;
 
-	    if (method === METHODS.GET || !data) {
-	      xhr.send();
-	    } else {
-	      xhr.send(data);
-	    }
-	  });
+			if (method === METHODS.GET || !data) {
+				xhr.send();
+			} else {
+				xhr.send(data);
+			}
+		});
 	};
 }
 
