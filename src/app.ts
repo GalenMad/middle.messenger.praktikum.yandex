7c7179d4c68a5e297276892a1f36f6830da9c437
Anton Voronkov <amvoronkov@yandex.ru>
2021-11-03 02:45:51 +0300

    linting

--- src/app.ts
+++ src/app.ts
@@ -5,25 +5,26 @@ import Router from './modules/router';
 const root: HTMLElement | null = document.querySelector('#app');
 
 if (root === null) {
-  throw new Error('Не найден root-элемент');
+    throw new Error('Не найден root-элемент');
 }
 
 const router = new Router({
-  mode: 'hash',
-  root: '/',
+    mode: 'hash',
+    root: '/',
 });
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 pages.forEach((page: { path: string, render: Function }): void => {
-  const { render, path } = page;
-  router.add(path, () => {
-    root.innerHTML = '';
-    root.appendChild(render());
-  });
+    const { render, path } = page;
+    router.add(path, () => {
+        root.innerHTML = '';
+        root.appendChild(render());
+    });
 });
 
 // TODO: Временная заглушка для разводящей страницы;
 router.add('', (): void => {
-  root.innerHTML = '';
-  root.appendChild(SpreadPage({ list: pages }));
+    root.innerHTML = '';
+    root.appendChild(SpreadPage({ list: pages }));
 });
+
