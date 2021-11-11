ea605ad2727894ec76805c2cbf06b5eff66003f2
Anton Voronkov <amvoronkov@yandex.ru>
2021-11-11 21:32:48 +0300

    base api

---
+++ src/api/auth.api.ts
@@ -0,0 +1,25 @@
+import HTTP from './fetch';
+import BaseAPI from './base.api';
+
+const authAPIInstance = new HTTP('/chats');
+
+export class LoginAPI extends BaseAPI {
+  create() {
+
+  }
+  request() {
+    return authAPIInstance;
+  }
+}
+
+export class LogoutAPI extends BaseAPI {
+  request() {
+    return authAPIInstance;
+  }
+}
+
+export class UserInfoAPI extends BaseAPI {
+  request() {
+    return authAPIInstance;
+  }
+}
---
+++ src/api/chats.api.ts
@@ -0,0 +1,15 @@
+import HTTP from './fetch';
+import BaseAPI from './base.api';
+
+const chatsAPIInstance = new HTTP('/chats');
+
+export default class ChatsAPI extends BaseAPI {
+
+  create() {
+    return chatsAPIInstance;
+  }
+
+  request() {
+    return chatsAPIInstance;
+  }
+}
