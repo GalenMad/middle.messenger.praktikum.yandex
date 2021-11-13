0122d35ea4b012250aa4828d414fd168d158b27d
Anton Voronkov <amvoronkov@yandex.ru>
2021-11-13 21:44:57 +0300

    upd

--- src/modules/controllers/base.controller.ts
+++ src/modules/controllers/base.controller.ts
@@ -1,6 +1,11 @@
 import Router from '../router';
 import EventBus from '../event-bus';
 
+interface errorData {
+  status: string | number,
+  data: { reason?: string }
+}
+
 export default class BaseController {
   eventBus: EventBus;
   router: Router;
@@ -9,7 +14,7 @@ export default class BaseController {
     this.router = new Router();
   }
 
-  throwError(title: string, response) {
+  throwError(title: string, response: errorData) {
     const { data, status } = response;
     throw new Error(`\n ref: ${title} \n status: ${status} \n reson: ${data.reason || data}`);
   }
