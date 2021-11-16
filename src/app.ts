92815eb371757465cf3c493c5684dec4e0455ebe
Anton Voronkov <amvoronkov@yandex.ru>
2021-11-16 22:05:01 +0300

    todos

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import Router from './modules/router';
 import AuthController from './modules/controllers/auth.controller';
 
 // TODO: Прикрутить алиасы когда будет вебпак
-
+// TODO: Попробовать организовать глобальные события, чтобы чистить валидацию, хайдить модалки и т.д.
 pages.forEach(({block, path, props = {}, options = {}}) => Router.use(path, block, props, options));
 new AuthController().init();
+
