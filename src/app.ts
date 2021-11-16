290ec2f92b67db68aaf0417fc1d9d4d5b666b90d
Anton Voronkov <amvoronkov@yandex.ru>
2021-11-16 14:25:40 +0300

    fix

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import pages from './pages';
 import Router from './modules/router';
 import AuthController from './modules/controllers/auth.controller';
 
-// TODO: Прикрутить алиасы
+// TODO: Прикрутить алиасы когда будет вебпак
 
 pages.forEach(({block, path, props = {}, options = {}}) => Router.use(path, block, props, options));
 new AuthController().init();
