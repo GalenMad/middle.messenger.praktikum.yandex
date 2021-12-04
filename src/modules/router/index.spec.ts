b4be60d581bd78e82f0744b3cfc64c881a1807de
Anton Voronkov <amvoronkov@yandex.ru>
2021-12-04 21:51:40 +0300

    rename

--- src/modules/router/index.spec.ts
+++ src/modules/router/index.spec.ts
@@ -1,8 +1,24 @@
-// describe('Роутер', () => {
-//   // Проверка отправки на 404
-//   // Проверка отправки на авторизацию
-//   // Проверка отправки к чатам
-//   // Проверка перехода на страницу
-//   // Проверка back
-//   // Проверка forward
-// });
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
+import { expect } from 'chai';
+import pages from '../../pages';
+import Router from './index';
+
+describe('Роутер', () => {
+  const isAuthorized = () => false;
+  pages.forEach(({
+    block, path, options = {},
+  }) => Router.use(path, block, options));
+  Router.start(isAuthorized);
+
+  // it('404', () => {
+  Router.go('/404');
+  console.log(window.history);
+  // });
+  // Проверка отправки на 404
+  // Проверка отправки на авторизацию
+  // Проверка отправки к чатам
+  // Проверка перехода на страницу
+  // Проверка back
+  // Проверка forward
+});
