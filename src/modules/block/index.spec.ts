1334ed0c18fbd36669c5b67d558ae7851f530486
Anton Voronkov <amvoronkov@yandex.ru>
2021-12-06 18:30:56 +0300

    upd

--- src/modules/block/index.spec.ts
+++ src/modules/block/index.spec.ts
@@ -3,7 +3,7 @@
 import { expect } from 'chai';
 import Block from './index';
 
-describe('Родительский класс Block', () => {
+describe('Модуль Block.', () => {
   let clickCounter = 0;
   let antoherClickCounter = 0;
 
@@ -23,48 +23,54 @@ describe('Родительский класс Block', () => {
   const block = new Block(tagName, props, children, selectors);
   const element = block.getContent();
 
-  it('Корректный тег', () => {
-    expect(element.tagName.toLowerCase()).to.equal(tagName);
+  describe('Генерация обёртки:', () => {
+    it('Корректный тег', () => {
+      expect(element.tagName.toLowerCase()).to.equal(tagName);
+    });
+
+    it('Присвоение атрибутов', () => {
+      expect(element).to.have.property('id');
+      expect(element).to.have.property('className');
+      expect(element.dataset).to.have.property('id');
+    });
   });
 
-  it('Присвоение атрибутов', () => {
-    expect(element).to.have.property('id');
-    expect(element).to.have.property('className');
-    expect(element.dataset).to.have.property('id');
+  describe('Работа с пропсами:', () => {
+    it('Наличие пропса', () => {
+      expect(block.props).to.have.property('foo');
+      expect(block.props.foo).to.equal('bar');
+    });
+
+    it('Перезапись пропса', () => {
+      block.setProps({ foo: 1 });
+      expect(block.props.foo).to.equal(1);
+    });
+
+    it('Наличие пропса из селектора', () => {
+      expect(block.props).to.have.property('data');
+      expect(block.props.data).to.equal('data');
+    });
   });
 
-  it('Наличие пропса', () => {
-    expect(block.props).to.have.property('foo');
-    expect(block.props.foo).to.equal('bar');
-  });
+  describe('Работа с событиями:', () => {
+    it('Срабатывание слушателей', () => {
+      element.click();
+      expect(clickCounter).to.equal(1);
+    });
 
-  it('Перезапись пропса', () => {
-    block.setProps({ foo: 1 });
-    expect(block.props.foo).to.equal(1);
-  });
+    it('Перезапись слушателей', () => {
+      const anotherEvents = [{
+        type: 'click',
+        cb: () => { antoherClickCounter += 1; },
+      }];
+      block.setProps({ events: anotherEvents });
+      element.click();
+      expect(antoherClickCounter).to.equal(1);
+    });
 
-  it('Наличие пропса из селектора', () => {
-    expect(block.props).to.have.property('data');
-    expect(block.props.data).to.equal('data');
-  });
-
-  it('Срабатывание слушателей', () => {
-    element.click();
-    expect(clickCounter).to.equal(1);
-  });
-
-  it('Перезапись слушателей', () => {
-    const anotherEvents = [{
-      type: 'click',
-      cb: () => { antoherClickCounter += 1; },
-    }];
-    block.setProps({ events: anotherEvents });
-    element.click();
-    expect(antoherClickCounter).to.equal(1);
-  });
-
-  it('Отсутствие срабатывания перезаписанных слушателей', () => {
-    expect(clickCounter).to.equal(1);
+    it('Отсутствие срабатывания перезаписанных слушателей', () => {
+      expect(clickCounter).to.equal(1);
+    });
   });
 
   // TODO: Доп. тесты Block
